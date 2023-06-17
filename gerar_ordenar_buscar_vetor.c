7e0af0a380aa863e40c25d5a170077199419223d
otaviozerotwo <deadplatypus@protonmail.com>
2023-06-17 12:07:04 -0300

    implementação condicionais de acesso opções do menu

--- gerar_ordenar_buscar_vetor.c
+++ gerar_ordenar_buscar_vetor.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+
+int vetor_gerado = 0;
+int vetor_ordenado = 0;
 
 void mostrar_menu(){
     char op;
@@ -32,37 +36,67 @@ void mostrar_menu(){
             break;
         case 'e':
         case 'E':
-            break;
+            exit(0);
         default:
             printf("Opcao invalida!\n\n");
             break;
         }
-    }while(op != 'e');
+    }while(op != 'e' || op != 'E');
 }
 
 int gerar_vetor(){
+    int i = 0;
     int tamanho = 0;
 
     printf("Digite o tamanho do vetor de inteiros: ");
     scanf("%d", &tamanho);
 
-    return 0;
+    int vetor[tamanho];
+
+    srand(time(NULL));
+
+    for(i = 0; i < tamanho; i++){
+        vetor[i] = rand();
+    }
+
+    for(i = 0; i < tamanho; i++){
+        printf("%d\n", vetor[i]);
+    }
+
+    printf("\n");
+
+    return vetor_gerado++;
 }
 
-void ordenar_vetor(){
+int ordenar_vetor(){
+    if(vetor_gerado > 0){
 
+        return vetor_ordenado++;
+    }else{
+        printf("Eh necessario gerar o vetor antes de ordena-lo!\n\n");
+
+        return vetor_ordenado;
+    }
 }
 
 void gerar_busca_sequencial(){
-
+    if(vetor_gerado > 0){
+        printf("%d\n", vetor_gerado);
+    }else{
+        printf("Eh necessario gerar o vetor antes de fazer a busca sequencial!\n\n");
+    }
 }
 
 void gerar_busca_binaria(){
-
+    if(vetor_gerado > 0 && vetor_ordenado > 0){
+        printf("%d\n", vetor_gerado);
+        printf("%d\n", vetor_ordenado);
+    }else{
+        printf("Eh necessario gerar e ordenar o vetor antes de fazer a busca binaria!\n\n");
+    }
 }
 
 int main(){
-
     mostrar_menu();
     return 0;
 }
